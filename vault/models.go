ae03443177ba42d0de266c622a81f176ce79da78
Your Name <you@example.com>
2026-05-22 10:04:26 +0545

    many updates

--- vault/models.go
+++ vault/models.go
@@ -1,17 +1,27 @@
 package vault
 
 type Credential struct {
-	ID        string `json:"id"`
-	Type      string `json:"type"`
-	URL       string `json:"url"`
-	Username  string `json:"username"`
-	Password  string `json:"password"`
-	Port      int    `json:"port"`
-	KeyFile   string `json:"key_file"`
-	SecretKey string `json:"secret_key"`
-	Notes     string `json:"notes"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	ID           string `json:"id"`
+	Type         string `json:"type"`
+	Title        string `json:"title"`
+	Description  string `json:"description"`
+	URL          string `json:"url"`
+	Username     string `json:"username"`
+	Password     string `json:"password"`
+	Port         int    `json:"port"`
+	KeyFile      string `json:"key_file"`
+	SecretKey    string `json:"secret_key"`
+	Notes        string `json:"notes"`
+	Service      string `json:"service"`
+	InstanceName string `json:"instance_name"`
+	PublicIP     string `json:"public_ip"`
+	Remarks      string `json:"remarks"`
+	AWSAccount   string `json:"aws_account"`
+	AWSRegion    string `json:"aws_region"`
+	OSService    string `json:"os_service"`
+	SSHCol       string `json:"ssh_col"`
+	CreatedAt    string `json:"created_at"`
+	UpdatedAt    string `json:"updated_at"`
 }
 
 type VaultData struct {
